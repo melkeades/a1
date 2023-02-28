3e00d1c55e4e2eb4e6f411b3d2a3e3649c4e02b5
melkeades <118681335+melkeades@users.noreply.github.com>
2023-03-01 00:44:23 +0300

    03/01 0

--- src/about.js
+++ src/about.js
@@ -48,57 +48,22 @@ captionsToScroll.forEach((cap) => {
     },
   })
 })
+let mm = gsap.matchMedia()
+let imgYmove = '-20%'
+mm.add('(max-width: 768px)', () => {
+  imgYmove = '0'
+})
 imgsToScroll.forEach((img) => {
-  // const caption = img.getElementsByClassName('text-img__img__caption')
-
-  // if (caption) {
-  //   console.log(caption)
-  //   gsap.to(caption, {
-  //     y: '-120%',
-  //     opacity: 0,
-  //     scrollTrigger: {
-  //       trigger: img,
-  //       scrub: true,
-  //     },
-  //   })
-  // }
   gsap.to(img, {
-    backgroundPositionY: '-20%',
+    backgroundPositionY: imgYmove,
+
     scrollTrigger: {
       trigger: img,
       scrub: true,
     },
   })
-  // console.log()
-  // .getElementsByClassName('text-img__img__caption')
-  // gsap.to(img, {
-  //   backgroundPositionY: '-120%',
-  //   scrollTrigger: {
-  //     trigger: img,
-  //     scrub: true,
-  //   },
 })
 
-// const aboutImg1scroll = gsap.timeline({ defaults: { ease: 'none' } })
-
-// aboutImg1scroll
-//   .from('.text-img__img', {
-//     backgroundPositionY: '170%',
-//   })
-//   .to('.text-img__img__caption', {
-//     y: '-100%',
-//   })
-
-// ScrollTrigger.create({
-//   // markers: true,
-//   trigger: '.text-img__img',
-//   start: 'top bottom',
-//   end: 'bottom top',
-//   animation: aboutImg1scroll,
-//   scrub: 1,
-//   // pin: true,
-//   // pinSpacing: false,
-// })
 const careerSlider = new Swiper('.my-career__swiper', {
   speed: 600,
   spaceBetween: 0,
@@ -116,33 +81,3 @@ const careerSlider = new Swiper('.my-career__swiper', {
     dragClass: 'my-career__nav__scrollbar',
   },
 })
-
-// let blogSlider
-// const blogSliderInit = () => {
-//   blogSlider = new Swiper('.blog__swiper', {
-//     speed: 600,
-//     spaceBetween: 40,
-//     slidesPerView: 1,
-//     slidesPerGroup: 1,
-//     pagination: {
-//       el: '.swiper-pagination',
-//     },
-//     navigation: {
-//       nextEl: '.blog__rightarrow',
-//       prevEl: '.blog__leftarrow',
-//     },
-//   })
-// }
-// if (window.innerWidth <= 767) {
-//   blogSliderInit()
-// }
-// const blogMq = window.matchMedia('(max-width: 767px)')
-// blogMq.onchange = (e) => {
-//   if (e.matches) {
-//     // 767 or less
-//     blogSliderInit()
-//   } else {
-//     // more than 767
-//     blogSlider.destroy()
-//   }
-// }
